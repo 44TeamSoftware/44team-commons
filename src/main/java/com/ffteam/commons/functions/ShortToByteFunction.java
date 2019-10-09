9dd468e1c75279e31c788f54e079a7830f4e101f
Maria Yatskiv <mikamause99@gmail.com>
2019-10-09 21:29:25 +0300

    Removed test method from ShortToByteFunction.java. Added test class for it. But without tests.

--- src/main/java/com/ffteam/commons/functions/ShortToByteFunction.java
+++ src/main/java/com/ffteam/commons/functions/ShortToByteFunction.java
@@ -1,16 +1,8 @@
 package com.ffteam.commons.functions;
-
+​
+import java.util.function.Function;
+​
 @FunctionalInterface
-public interface ShortToByteFunction {
-	byte applyAsByte(short s);
-}
-
-class TestShortToByteFunction {
-	public byte[] transformArray(short[] array, ShortToByteFunction function) {
-		byte[] transformedArray = new byte[array.length];
-		for (int i = 0; i < array.length; i++) {
-			transformedArray[i] = function.applyAsByte(array[i]);
-		}
-		return transformedArray;
-	}
+public interface ShortToByteFunction extends Function<Short, Byte> {
+	
 }
