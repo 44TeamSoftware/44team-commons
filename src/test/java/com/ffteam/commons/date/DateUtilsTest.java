1345d6d628511e429e78f289144244ce51f794ff
Katheryn Osmond <katheryn.osmond@excella.com>
2019-10-09 13:59:10 -0400

    Added unit tests for compare methods

--- src/test/java/com/ffteam/commons/date/DateUtilsTest.java
+++ src/test/java/com/ffteam/commons/date/DateUtilsTest.java
@@ -54,4 +54,73 @@ public class DateUtilsTest {
 
 		assertEquals(expected, actual);
 	}
+
+	@Test
+	public void testCompareLocalDate_withBothDatesNull() {
+		LocalDate date1 = null;
+		LocalDate date2 = null;
+		int expected = 0;
+
+		int actual = DateUtils.compare(date1, date2);
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testCompareLocalDate_withDateOneNull() {
+		LocalDate date1 = null;
+		LocalDate date2 = LocalDate.of(2000, 12, 3);
+		int expected = -1;
+
+		int actual = DateUtils.compare(date1, date2);
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testCompareLocalDate_withDateTwoNull() {
+		LocalDate date1 = LocalDate.of(2000, 12, 3);
+		LocalDate date2 = null;
+		int expected = 1;
+
+		int actual = DateUtils.compare(date1, date2);
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testCompareDate_withBothDatesNull() {
+		Date date1 = null;
+		Date date2 = null;
+		ZoneId zoneId = ZoneId.of("EST", ZoneId.SHORT_IDS);
+		int expected = 0;
+
+		int actual = DateUtils.compare(zoneId, date1, date2);
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testCompareDate_withDateOneNull() {
+		Date date1 = null;
+		Date date2 = new Date(2000-1900, 11, 3);
+		ZoneId zoneId = ZoneId.of("EST", ZoneId.SHORT_IDS);
+		int expected = -1;
+
+		int actual = DateUtils.compare(zoneId, date1, date2);
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testCompareDate_withDateTwoNull() {
+		Date date1 =  new Date(2000-1900, 11, 3);
+		Date date2 = null;
+		ZoneId zoneId = ZoneId.of("EST", ZoneId.SHORT_IDS);
+		int expected = 1;
+
+		int actual = DateUtils.compare(zoneId, date1, date2);
+
+		assertEquals(expected, actual);
+	}
 }
